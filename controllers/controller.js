118df7fa955b6b77fb9f1f1eb6acf47b1a4d12fd
cyril-deguzman <cyrilethan.deguzman@gmail.com>
2021-07-24 16:01:12 +0800

    added controller.js and routes.js

---
+++ controllers/controller.js
@@ -0,0 +1,18 @@
+const {render} = require('../routes/routes.js');
+
+const controller = {
+
+    /**
+     * getIndex.
+     * 
+     * renders the homepage.
+     * @param {*} req 
+     * @param {*} res 
+     */
+    getIndex: function(req, res) {
+        res.render('index', {});
+    },
+
+}
+
+module.exports = controller;
