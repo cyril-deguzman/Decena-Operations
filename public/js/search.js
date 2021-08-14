6b37391a20be97b9ea2e79e63df477ca758ec06c
Chadi <chadi_turk@dlsu.edu.ph>
2021-08-14 12:00:31 +0800

    Updated search.hbs: added card to count the number of results returned by the search

--- public/js/search.js
+++ public/js/search.js
@@ -1,3 +1,37 @@
-// TODO
-// 1. When searching for a company, and the results return more than one, 
-//  a new card should be added don't let it stack up in one card.
+$(document).ready(function () {
+    
+    // This function just toggles the gray background color for the card
+    $(".resultsFoundCard").hover(function(){
+        $(this).children().css("background-color","#dbdbdb");
+      },
+      function(){
+        $(this).children().css("background-color","#FFFFFF");
+      });
+
+
+      /*
+      This checks if the search is able to return a company, if succcessful the card will 
+      display the number of companies matching the search query.*/
+      
+      var url = window.location.href;
+      var searchValue = url.split('=')[1];
+
+      if ($(".resultsFoundCard")[0]){
+        var numberOfCompaniesSearch = $(".resultsFoundCard").length; //returns number of companies
+        numberOfCompaniesSearch = numberOfCompaniesSearch.toString();
+
+        $("#resultsCountCard").text("There are ("+ numberOfCompaniesSearch +")" + " results found for: " + searchValue);
+
+    } else {
+
+        $("#resultsCountCard").hide();
+
+        if (window.location.href.indexOf("?") != -1){
+            $("#noResultsFoundCard").text("There are (0) results found for: " +searchValue);
+            $("#noResultsParentCard").css("background-color","#ff7369");
+        }   
+        else{
+            $("#noResultsFoundCard").text("No search has been made yet.");
+        }        
+    }
+})
