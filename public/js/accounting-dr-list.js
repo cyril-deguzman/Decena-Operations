bbd73110942f68d0ea1a5b39e7b50ed19935eb35
Chadi <chadi_turk@dlsu.edu.ph>
2021-08-31 02:00:02 +0800

    created accounting-dr-list.hbs page

---
+++ public/js/accounting-dr-list.js
@@ -0,0 +1,148 @@
+$(document).ready(function() {
+
+
+    /*
+        Change style of div based on payment status
+    */
+
+
+    var paymentStatus = $(".paymentStatus");
+    var paid = "paid";
+    var pending = "pending";
+    
+    paymentStatus.each(function(){
+        if($(this).text().toUpperCase() == paid.toUpperCase()){
+            $(this).removeClass("paymentStatus");
+            $(this).addClass("paidStatus");
+            $(this).text("Paid");
+            $(this).off('hover');
+            $(this).prop("disabled",true);
+
+        }
+        else if($(this).text().toUpperCase() == pending.toUpperCase()){
+            // $(this).css("background-color","#545454");
+            $(this).text("Pending");
+        }
+    });
+
+    
+    var title;
+
+    // Setup - add a text input to each footer cell
+    $('.drDataTable tfoot th.filterCol').each( function () {
+        title = $(this).text();
+        $(this).html( '<input type="text" placeholder="'+ " " + title +'" class ="searchField" />' );
+    } );
+ 
+    // DataTable
+    var table = $('.drDataTable').DataTable({  bAutoWidth: false,
+        columnDefs: [
+            { orderable: false, targets: -2 }
+         ],
+        aoColumns : [
+          { sWidth: '140px' },
+          { sWidth: '150px' },
+          { sWidth: '150px' },
+          { sWidth: '120px' },
+          { sWidth: '150px' },
+          { sWidth: '90px' },
+          { sWidth: '100px' }
+        ],
+        initComplete: function () {
+            // Apply the search
+            this.api().columns().every( function () {
+                var that = this;
+ 
+                $( 'input', this.footer() ).on( 'keyup change clear', function () {
+                    if ( that.search() !== this.value ) {
+                        that
+                            .search( this.value )
+                            .draw();
+                    }
+                } );
+            } );
+        }
+    });
+
+    /*
+        This is used to append the search fields to the header since it's originally
+        placed in the footer.
+    */
+
+    $('.drDataTable tfoot tr').appendTo('.drDataTable thead');
+    $('.dataTables_filter input[type="search"]').css(
+        {'width':'50px','display':'inline-block'}
+     );
+
+     $('#myInputTextField').keyup(function(){
+        table.search($(this).val()).draw();
+    })
+    /*
+        This function gets all of the search input fields and adjusts the width of each.
+    */
+     var searchFields = $(".searchField");
+        for(var i=0; i<searchFields.length; i++){
+            var instanceOfSearchField= searchFields.eq(i);
+            
+            switch(i)
+            {
+                // case 0:
+                   
+                case 1:
+                    instanceOfSearchField.css("width","160px");
+                    break;
+                case 2:
+                    instanceOfSearchField.css("width","170px");
+                    break;
+                case 3:
+                    instanceOfSearchField.css("width","170px");
+                    break;
+                case 4:
+                    instanceOfSearchField.css("width","140px");
+                    break;
+                case 5:
+                    instanceOfSearchField.css("width","170px");
+                    break;
+                case 6:
+                    instanceOfSearchField.css("width","123px");
+                    break;
+            }
+        }
+
+        //This just removes the text for the Edit DR column
+        $('th').eq(12).text("");
+        
+        //Adds an ID "searchDrForm" to the search input to resize it
+        $("label").eq(1).attr('id','searchDrForm');
+        $("input[type='search']").css("width","200px");
+        $("label").eq(2).css('position','relative');
+        $("label").eq(2).css('bottom','20px');
+        $("label").eq(2).css('right','10px');
+
+        /* Filter Year */
+        $.fn.dataTable.ext.search.push(
+            function( settings, data, dataIndex ) {
+                var year = $('#search-year-input').val();
+                var date = ( data[0] ) || 0; // use data for the age column
+        
+                if ( date.includes(year))
+                    return true;
+        
+                return false;
+            }
+        );
+    
+        $(document).ready(function() {
+            var table = $('#dr-datatable').DataTable();
+            
+            // Event listener filtering inputs to redraw on input
+            $('#search-year-input').keyup( function() {
+                table.draw();
+            });
+        });
+
+       
+
+        
+        
+} );
